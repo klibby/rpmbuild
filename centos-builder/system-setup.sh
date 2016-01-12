3a98b502d640ebcf953831a6a2f27966c91f9fdd
kendall libby <klibby@mozilla.com>
2016-01-12 14:53:11 -0500

    add fpm, cpan2rpm and deps; better url for cpanm

--- centos-builder/system-setup.sh
+++ centos-builder/system-setup.sh
@@ -6,14 +6,28 @@ test `whoami` == 'root';
 yum_packages=()
 
 # Dependencies for setup
+yum_packages+=('which')
 yum_packages+=('wget')
 yum_packages+=('curl')
+yum_packages+=('tar')
+yum_packages+=('gcc')
+yum_packages+=('perl-core')
+yum_packages+=('rpm-build')
+yum_packages+=('ruby-devel')
+yum_packages+=('rubygems')
 
 yum update -y
 yum install -y ${yum_packages[@]}
 
 # Bootstrap cpanminus
-curl -L https://cpanmin.us | perl - App::cpanminus
+curl -L https://git.io/cpanm | perl - App::cpanminus
+
+# Same for cpan2rpm
+mkdir -p $HOME/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cpanm http://search.cpan.org/CPAN/authors/id/B/BB/BBB/cpan2rpm-2.028_02.tar.gz
+
+# Same for fpm
+gem install fpm
 
 ### Clean up from setup
 yum clean all
